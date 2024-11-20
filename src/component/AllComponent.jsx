7867c92ffa19048042410db64ac788980cf2b130
DarkDesignerSalman <salmanislam501@gmail.com>
2024-11-20 20:29:10 +0600

    Save current changes

--- src/component/AllComponent.jsx
+++ src/component/AllComponent.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import Header from "./Header";
 import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import Slider from "./Slider";
-import Footer from "./Footer";
-import Contact from "./Contact";
 import ServiceMain from "./ServiceMain";
 
 export default function AllComponent() {
